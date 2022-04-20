177e287931020e994e8f7b70235466ed0037297d
Mao Li <mli94@stevens.edu>
2022-04-20 17:55:51 -0400

    finished product SnakeGame

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,17 +1,17 @@
 import javax.swing.JFrame;
 
-public class GameFrame extends JFrame{
+public class GameFrame extends JFrame {
 
-	GameFrame() {
-		
-		GamePanel panel = new GamePanel();
-		
-		this.add(panel);
-		this.setTitle("Snake");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
-		this.pack();
-		this.setVisible(true);
-		this.setLocationRelativeTo(null);
-	}
+    GameFrame() {
+
+        GamePanel panel = new GamePanel();
+
+        this.add(panel);
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
