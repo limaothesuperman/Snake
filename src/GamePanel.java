6084aee41b9891dd8a66474a6439197a2380e754
Mao Li <mli94@stevens.edu>
2022-04-20 16:18:18 -0400

    new changes

---
+++ src/GamePanel.java
@@ -0,0 +1,51 @@
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+	GamePanel() {
+		
+	}
+	
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkApple() {
+		
+	}
+	
+	public void checkCollisions() {
+		
+	}
+	
+	public void gameOver(Graphics g) {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public class MykeyAdapter extends KeyAdapter {
+		
+	}
+
+}
